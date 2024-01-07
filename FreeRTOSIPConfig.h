ab75ac440fc21e5845bf54dcaf0e7d61aa4d7bc4
T.Omura <itp903nc@gmail.com>
2024-01-07 17:12:21 +0900

    FreeRTOS Plus TCPIP

---
+++ FreeRTOSIPConfig.h
@@ -0,0 +1,11 @@
+#ifndef FREERTOS_IP_CONFIG_H
+#define FREERTOS_IP_CONFIG_H
+
+#define ipconfigBYTE_ORDER pdLITTLE_ENDIAN
+#define ipconfigETHERNET_DRIVER_FILTERS_FRAME_TYPES 0
+#define ipconfigUSE_IPv4 0
+#define ipconfigUSE_IPv6 1
+#define ipconfigUSE_TCP 0
+#define ipconfigUSE_TCP_WIN 0
+
+#endif /* FREERTOS_IP_CONFIG_H */
